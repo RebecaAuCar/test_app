99f5b8d38e24dfcac556e904e208669f1bcc16d5
Rebeca <rebeca.ac@gmail.com>
2022-01-13 19:50:05 +0100

    complete exercise

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +11,8 @@ describe('HomePage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),
+      ReactiveFormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public loginForm: FormGroup;
+  public emailInvalid: boolean = false;
+  public passInvalid: boolean = false;
+   
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        pass: ['', [Validators.required, Validators.minLength(5)]],
+        reminder: [false]
+      }
+    );
+  }
 
-  constructor() {}
+  public clear(emailOrPass){
+    switch (emailOrPass){
+      case 'email': this.emailInvalid = false;
+      break;
+      case 'pass': this.passInvalid = false;
+      break;
+    }
+  }
 
+  public login(){
+      this.loginForm.get('email').invalid ? this.emailInvalid = true : this.emailInvalid = false;
+      this.loginForm.get('pass').invalid ? this.passInvalid = true : this.passInvalid = false;
+    if (!this.emailInvalid && !this.passInvalid){
+      console.log("Formulario enviado correctamente con los siguientes valores: ");
+      console.log("Email: " + this.loginForm.get('email').value);
+      console.log("Contraseña: " +this.loginForm.get('pass').value);
+      console.log("Recordar: " + this.loginForm.get('reminder').value);
+    }
+  } 
 }
